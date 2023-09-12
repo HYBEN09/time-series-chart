c4e8c746d3839e5b390ed5b0657f05e7aa608ec2
hyebeen byun <hyebeen2658@naver.com>
2023-09-13 08:19:46 +0900

    Fix: 불필요한 props가 DOM으로 전달되지 않도록 관리

--- src/components/UI/Buttons.tsx
+++ src/components/UI/Buttons.tsx
@@ -11,21 +11,22 @@ function Buttons({ onClick, activated, children }: IBtnProps) {
     <StyledButton
       type="button"
       onClick={onClick}
-      activated={activated ? 'true' : 'false'}
+      $activated={activated ? 'true' : 'false'}
     >
       {children}
     </StyledButton>
   );
 }
 
-const StyledButton = styled.button<{ activated: string }>`
+const StyledButton = styled.button<{ $activated: string }>`
   height: 2.3rem;
   border-radius: 8px;
   color: #fff;
   padding: 0 1rem;
   font-size: 1.2rem;
+
   background: ${(props) =>
-    props.activated === 'true' ? `#1f76c3` : `#00539C`};
+    props.$activated === 'true' ? `#1f76c3` : `#00539C`};
 `;
 
 export default Buttons;
